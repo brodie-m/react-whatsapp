3542867bf9197f02211adc8ddd636b7069286b4d
brodie-m <brodiedmcguire@gmail.com>
2021-10-09 10:30:23 +0100

    added boilerplate and login

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Login from './login'
+
+function App() {
+  const [id,setId] = useState()
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  )
+}
+
+export default App;
