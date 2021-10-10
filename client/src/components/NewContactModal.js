9537367d02ea680f10eed3caf8aafbca0f7aa415
brodie-m <brodiedmcguire@gmail.com>
2021-10-10 16:42:06 +0100

    finished contact modal

--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -1,12 +1,17 @@
 import React , {useRef} from 'react'
 import {Modal, Form, Button} from 'react-bootstrap'
 
-export default function NewContactModal() {
+export default function NewContactModal({closeModal}) {
     const idRef = useRef()
     const nameRef = useRef()
     function handleSubmit(e) {
         e.preventDefault();
+
+        // createContact(idRef.current.value,nameRef.current.value)
+        closeModal()
     }
+
+
 
     return (
         <>
@@ -21,6 +26,8 @@ export default function NewContactModal() {
                     <Form.Label>Name</Form.Label>
                     <Form.Control type = "text" ref = {nameRef} required/>
                 </Form.Group>
+
+                <Button type="submit">Create</Button>
             </Form>
             
         </Modal.Body>
