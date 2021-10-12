5db14a9e8c7c9b33b766e50aa862e2191fd0b264
brodie-m <brodiedmcguire@gmail.com>
2021-10-12 08:03:10 +0100

    conversations bug fix in progress

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {ListGroup} from 'react-bootstrap'
+import { useConversations } from '../contexts/ConversationsProvider'
+
+
 
 export default function Conversations() {
+    
+    const  conversationsData  = useConversations() ;
+    
+    console.log('logging convos', conversationsData)
+    const conversations = conversationsData.conversations
     return (
-        <div>
-            Conversations
-        </div>
-    )
+        <ListGroup variant="flush">
+            {conversations.map((conversation,index)=> (
+                <ListGroup.Item key = {index}>
+                    {conversation.recipients.map(r=> r.name).join(', ')}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    ) 
 }
