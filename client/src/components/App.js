dbadb0d5210f534ff38da5e6077057042d0f9fc4
brodie-m <brodiedmcguire@gmail.com>
2021-10-12 12:02:43 +0100

    made conversations work - added server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,25 +6,25 @@ import { ContactsProvider } from '../contexts/ContactsProvider'
 import { ConversationsProvider } from '../contexts/ConversationsProvider'
 
 function App() {
-  const [id,setId] = useLocalStorage('id')
+  const [id, setId] = useLocalStorage('id')
 
   const dashboard = (
     <>
-    <ContactsProvider>
-    <ConversationsProvider>
-    <Dashboard id={id}/>
-    </ConversationsProvider>
-      
-    </ContactsProvider>
-    
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+
+      </ContactsProvider>
+
     </>
   )
 
   return (
-    
-      id ? dashboard : <Login onIdSubmit={setId} />
-      
-   
+
+    id ? dashboard : <Login onIdSubmit={setId} />
+
+
   )
 }
 
